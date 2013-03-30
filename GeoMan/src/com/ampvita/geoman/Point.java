9e1bb5d9309d3e527f0cfa4c5670c17a2c898c6f
Daniel Gillespie <danielg@umd.edu>
2013-03-30 17:23:54 -0400

    Updated Major Classes

---
+++ GeoMan/src/com/ampvita/geoman/Point.java
@@ -0,0 +1,12 @@
+package com.ampvita.geoman;
+
+public class Point {
+	double xCoord;
+	double yCoord;
+	
+	public Point(double xIn, double yIn) {
+		this.xCoord = xIn;
+		this.yCoord = yIn;
+	}
+
+}
