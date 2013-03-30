94f29759f6c8f05b6b8548c56fa94f69de2aadc8
Daniel Gillespie <danielg@umd.edu>
2013-03-30 16:00:25 -0400

    Added GameGrid Class

---
+++ GeoMan/src/com/ampvita/geoman/GameGrid.java
@@ -0,0 +1,19 @@
+package com.ampvita.geoman;
+
+/*
+ * Parameters:
+ * size: x,y
+ * pacman spots: x,y list
+ * ghost spots: x,y list
+ * wall positions: x,y list
+ * power pellets: x,y list
+ * else default food pellet
+ * 
+ */
+
+public class GameGrid {
+	public int sizeX;
+	public int sizeY;
+	
+	
+}
