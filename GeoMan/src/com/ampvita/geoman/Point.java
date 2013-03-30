a029a4e1bdc79f6604cb386a162cbf62992e5401
Richard Higgins <relh@terpmail.umd.edu>
2013-03-30 17:46:18 -0400

    Modded Point.java

--- GeoMan/src/com/ampvita/geoman/Point.java
+++ GeoMan/src/com/ampvita/geoman/Point.java
@@ -3,7 +3,7 @@ package com.ampvita.geoman;
 public class Point {
 	double xCoord;
 	double yCoord;
-	
+	//test richard 123
 	public Point(double xIn, double yIn) {
 		this.xCoord = xIn;
 		this.yCoord = yIn;
