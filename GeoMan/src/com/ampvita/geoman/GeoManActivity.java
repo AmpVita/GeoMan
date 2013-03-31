eb8e99cd98a021d9d20bac0593f43428d4945776
Richard Higgins <relh@terpmail.umd.edu>
2013-03-31 10:14:33 -0400

    Made a launch screen with host/guest choice.
    Host/guest buttons redirect to new activities with QR codes on them.
    
    Current QR codes are just "group1" and "group1guest"
    
    Last Time I commit the whole directory I promise!

--- GeoMan/src/com/ampvita/geoman/GeoManActivity.java
+++ GeoMan/src/com/ampvita/geoman/GeoManActivity.java
@@ -2,7 +2,10 @@ package com.ampvita.geoman;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class GeoManActivity extends Activity {
 
@@ -10,6 +13,24 @@ public class GeoManActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geo_man);
+
+        final Intent host = new Intent(this, HostQR.class);
+        final Button hostButton = (Button) findViewById(R.id.host);
+        hostButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	//use putExtra for data
+            	startActivity(host);
+            }
+        });
+        
+        final Intent guest = new Intent(this, GuestQR.class);
+        final Button guestButton = (Button) findViewById(R.id.guest);
+        guestButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	//use putExtra for data
+            	startActivity(guest);
+            }
+        });
     }
 
     @Override
---
+++ GeoMan/src/com/ampvita/geoman/GuestQR.java
@@ -0,0 +1,22 @@
+package com.ampvita.geoman;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class GuestQR extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guest_qr);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.geo_man, menu);
+        return true;
+    }
+    
+}
