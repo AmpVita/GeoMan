4875effac7a5a3ef011d2151cf86ba2ac1db6a48
Daniel Gillespie <danielg@umd.edu>
2013-04-23 18:25:49 -0400

    Fksjdflksjfkl

--- GeoMan/src/com/ampvita/geoman/client/GeoManActivity.java
+++ GeoMan/src/com/ampvita/geoman/client/GeoManActivity.java
@@ -8,7 +8,6 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import org.msgpack.MessagePack;
 
 public class GeoManActivity extends Activity {
 
