5809b276c91a3949691621b821748882f4d13d5b
Daniel Gillespie <danielg@umd.edu>
2013-04-23 18:21:54 -0400

    Restored functionality

--- GeoMan/src/com/ampvita/geoman/client/GeoManActivity.java
+++ GeoMan/src/com/ampvita/geoman/client/GeoManActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import org.msgpack.MessagePack;
 
 public class GeoManActivity extends Activity {
 
